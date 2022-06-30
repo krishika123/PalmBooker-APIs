46e67e5eb97ba5f0153724313cfd75e7adb133da
krishika123 <mirpurikrishika10@gmail.com>
2022-06-30 12:26:51 +0000

    seventh commit

--- KrishBookingAPI/Program.cs
+++ KrishBookingAPI/Program.cs
@@ -7,7 +7,13 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Add services to the container.
 builder.Services.AddDbContext<KRISHBOOKINGDBContext>(options =>
     options.UseSqlServer(builder.Configuration["ConnectionStrings:PalmBookingDb"]));
-
+builder.Services.AddAuthentication("Bearer")
+   .AddJwtBearer("Bearer", opt =>
+   {
+       opt.RequireHttpsMetadata = false;
+       opt.Authority = "https://psl-app-vm3/EBookkeepingAuth";
+       opt.Audience = "ebookkeeping-api";
+   });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,6 +30,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
