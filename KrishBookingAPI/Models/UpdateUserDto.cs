b145a2a757b7767d9c2f2d06b7fc1c62f8b97b1d
krishika123 <mirpurikrishika10@gmail.com>
2022-07-05 15:51:37 +0000

    ninth commit

---
+++ KrishBookingAPI/Models/UpdateUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace KrishBookingAPI.Models
+{
+    public class UpdateUserDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ KrishBookingAPI/Profiles/UserProfile.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using KrishBookingAPI.Entities;
+using KrishBookingAPI.Models;
+
+namespace KrishBookingAPI.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UpdateUserDto, User>()
+                .ForMember(
+                    dest => dest.Password,
+                    opt => opt.MapFrom(src => $"{src.Password}")
+                )
+                ;
+
+            CreateMap<CreateUserDto, User>()
+                .ReverseMap();
+                ;
+
+            CreateMap<UserDetailsDto, User>()
+                .ForMember(
+                    dest => dest.Contacts,
+                    opt => opt.MapFrom(src => src.Contacts)
+                ).ReverseMap();
+                ;
+
+            CreateMap<BookingDto, Booking>()
+                .ForMember(
+                    dest => dest.Id,
+                    opt => opt.MapFrom(src => src.BookingId)
+                ).ReverseMap();
+                ;
+
+            CreateMap<ContactDto, Contact>()
+                .ForMember(
+                    dest => dest.Id,
+                    opt => opt.MapFrom(src => src.ContactId)
+                ).ReverseMap();
+            ;
+
+        }
+    }
+}
